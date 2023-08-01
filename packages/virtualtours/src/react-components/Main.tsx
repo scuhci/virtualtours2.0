8b3bf4e7e3127d33e1e38e37caba95af2c39086b
MLi-dev <limatthew2006@gmail.com>
2023-08-01 12:13:59 -0700

    Added stork plan to fly user back to start.

--- packages/virtualtours/src/react-components/Main.tsx
+++ packages/virtualtours/src/react-components/Main.tsx
@@ -221,6 +221,15 @@ export class MainView extends Component<Props, State> {
       y: 0.00001,
       z: 0.00001,
     }, { x: -32.678383074276525, y: 0.3582876760621081, z: -26.83219463891109 });
+    this.addSceneObject(this.sdk, 'mp.gltfLoader', 'https://cdn.jsdelivr.net/gh/mrdoob/three.js@dev/examples/models/gltf/Stork.glb', {
+      x: 0.01,
+      y: 0.01,
+      z: 0.01,
+    }, {
+      x: 1.2523820905004721,
+      y: 1.95019005476538944,
+      z: 4.932051502749859,
+    });
     this.addGeoObject();
     const initObj = {
       size: 1.5,
