cea9c9dff1703ec35a6e7d1d2119fac73216aea3
MLi-dev <limatthew2006@gmail.com>
2023-08-01 11:00:41 -0700

    Combined the index and root files

--- packages/virtualtours/src/index.tsx
+++ packages/virtualtours/src/index.tsx
@@ -3,13 +3,16 @@ import * as ReactDOMClient from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/landing.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import Root from './root';
+import App from './react-components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 const initialize = async () => {
   const container = document.getElementById("content");
   const root = ReactDOMClient.createRoot(container);
   root.render(
-    <Root />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   );
 };
 initialize(); 
