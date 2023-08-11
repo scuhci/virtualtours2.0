4cb9f1da749c5debb1a97c1f141235545244e383
MYLi <myli@scu.edu>
2023-08-10 19:27:22 -0700

    Added to log in component, will send to firebase

--- packages/virtualtours/src/react-components/pages/Login.tsx
+++ packages/virtualtours/src/react-components/pages/Login.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../util/firebaseUtil'
 
-class Login extends React.Component {
-	render() {
-		return (
+
+const Login = () => {
+	const logGoogleUser = async () => {
+		const { user } = await signInWithGooglePopup();
+		const userDocRef = await createUserDocumentFromAuth(user);
+	}
+	return (
+		<>
 			<div className='card' style={{ marginTop: "20px" }}>
 				<div className='card-body'>Login</div>
+				<button onClick={logGoogleUser}>Sign in with Google Popup</button>
 			</div>
-		);
-	}
+		</>
+	);
 }
 
 export default Login;
---
+++ packages/virtualtours/src/util/firebaseUtil.ts
@@ -0,0 +1,44 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDw5hS2AcfPeBe7SX1u-7lbXLU09V0FnDA",
+	authDomain: "virtualtours-db.firebaseapp.com",
+	projectId: "virtualtours-db",
+	storageBucket: "virtualtours-db.appspot.com",
+	messagingSenderId: "193995438873",
+	appId: "1:193995438873:web:e0f8215e97e442f4b6a388",
+};
+
+const app = initializeApp(firebaseConfig);
+console.log(app);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth: any) => {
+	const userDocRef = doc(db, "users", userAuth.uid);
+	console.log(userDocRef);
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot.exists());
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log("error creating the user", error.message);
+		}
+	}
+	return userDocRef;
+};
