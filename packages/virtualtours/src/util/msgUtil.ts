ee39667dbc6bd24521888d65560cec2750dc2a50
MYLi <myli@scu.edu>
2023-08-14 14:40:23 -0700

    When sign in will take user to homepage and display message.

--- packages/virtualtours/src/util/msgUtil.ts
+++ packages/virtualtours/src/util/msgUtil.ts
@@ -4,8 +4,19 @@ export const setMessage = function (message: string) {
 	element.classList.add("visible");
 	element.innerText = message;
 };
+export const signInMessage = function (message: string) {
+	const element = document.getElementById("signintext");
+	element.classList.remove("hidden");
+	element.classList.add("visible");
+	element.innerText = message;
+};
 export const clearMessage = function () {
 	const element = document.getElementById("text");
+	element.classList.remove("visible");
+	element.classList.add("hidden");
+};
+export const clearSignInMessage = function () {
+	const element = document.getElementById("signintext");
 	element.classList.remove("visible");
 	element.classList.add("hidden");
 };
